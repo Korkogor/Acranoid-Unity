c49d6123863782b0cf372348de8b38c3ebeb850e
unknown <korkogor@gmail.com>
2026-02-03 15:19:43 +0300

    Platform increase bonus implemented

--- Assets/Scripts/Expand.cs
+++ Assets/Scripts/Expand.cs
@@ -7,18 +7,11 @@ public class Expand : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Transform platform = collision.gameObject.transform;
+        var platform = collision.gameObject.GetComponent<PlatformController>();
         if (collision.gameObject.CompareTag("Platform"))
         {
-            platform.localScale += new Vector3(0.3f, 0, 0);
-            StartCoroutine (PlatformExpand(2));
-        }
-        IEnumerator PlatformExpand(int value)
-        {
-            yield return new WaitForSeconds(value);
-            platform.localScale -= new Vector3(0.3f, 0, 0);
+            platform.PlatformExpandM();
             Destroy(gameObject);
-            
         }
     }
 }
--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformController : MonoBehaviour
@@ -6,12 +7,14 @@ public class PlatformController : MonoBehaviour
     private float horizontal;
     private Rigidbody2D rb;
     public Transform saveScale;
+    Vector3 originalScale;
     
     
     void Start()
     {
        rb = GetComponent<Rigidbody2D>();
        saveScale = GetComponent<Transform>();
+       originalScale = new Vector3(0.7F, 0.7F, 0.7F);
     }
 
     void Update()
@@ -25,6 +28,19 @@ public class PlatformController : MonoBehaviour
         
         rb.linearVelocity = new Vector2(horizontal * speed, 0);
     }
-    
+
+    public void PlatformExpandM()
+    {
+        transform.localScale += new Vector3(0.3f, 0, 0);
+        StartCoroutine(PlatformExpand(2f));
+        
+    }
+
+    IEnumerator<WaitForSeconds> PlatformExpand(float value)
+    {
+        yield return new WaitForSeconds(value);
+        transform.localScale = originalScale;
+    }
 
 }
+
